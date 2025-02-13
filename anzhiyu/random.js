2bdb9e7e5a9934a6f602c06fae40c1944257fae5
ClimbingMouse <wh18720283974@163.com>
2025-02-13 11:39:57 +0800

    Site updated: 2025-02-13 11:39:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/06/优秀文章-Android/","2025/01/07/复盘/","2025/01/10/好用的工具/","2025/01/10/笔记-Android/","2025/01/06/面试题-Android/","2024/12/21/小技巧/"];function toRandomPost(){
+var posts=["2025/01/06/优秀文章-Android/","2025/01/07/复盘/","2025/01/10/好用的工具/","2025/01/06/面试题-Android/","2025/01/10/笔记-Android/","2024/12/21/小技巧/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
