969ed5567719fcf0fc354437436d34d1ae7516e7
ClimbingMouse <wh18720283974@163.com>
2025-03-09 15:24:13 +0800

    Site updated: 2025-03-09 15:24:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/06/forinterview/","2025/03/09/getherback/","2025/03/02/englishlearn/","2025/03/01/justrecord/","2025/01/06/nicearticles/","2025/01/10/nicetools/","2025/01/10/notes/","2024/12/21/someskills/","2025/01/07/summary/"];function toRandomPost(){
+var posts=["2025/03/09/getherback/","2025/03/02/englishlearn/","2025/01/06/forinterview/","2025/03/01/justrecord/","2025/01/06/nicearticles/","2025/01/10/nicetools/","2024/12/21/someskills/","2025/01/10/notes/","2025/01/07/summary/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
