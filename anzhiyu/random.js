aae26e1fcce313ad8c057d556d911294f0c664cb
ClimbingMouse <wh18720283974@163.com>
2025-10-27 11:27:08 +0800

    Site updated: 2025-10-27 11:26:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/24/contentprovider/","2025/03/22/GradleLearn/","2025/03/27/decodeapk/","2025/08/12/excelttransfer/","2025/01/06/forinterview/","2025/03/02/englishlearn/","2025/03/01/justrecord/","2025/08/15/kotlin/","2025/01/06/nicearticles/","2025/01/10/nicetools/","2024/12/21/someskills/","2025/01/10/notes/","2025/01/07/studyroad/","2025/01/07/summary/","2025/10/27/【每日早报】/","2025/08/29/考研常识/","2025/10/22/changekey/"];function toRandomPost(){
+var posts=["2025/03/22/GradleLearn/","2025/10/22/changekey/","2025/03/27/decodeapk/","2025/07/24/contentprovider/","2025/03/02/englishlearn/","2025/08/12/excelttransfer/","2025/01/06/forinterview/","2025/03/01/justrecord/","2025/08/15/kotlin/","2025/01/10/nicetools/","2025/01/06/nicearticles/","2025/01/10/notes/","2025/01/07/studyroad/","2024/12/21/someskills/","2025/01/07/summary/","2025/10/27/【每日早报】/","2025/08/29/考研常识/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
