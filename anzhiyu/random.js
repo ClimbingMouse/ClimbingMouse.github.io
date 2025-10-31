1e52151d3479ca8a1a3a79682f106ee0cef99b50
ClimbingMouse <wh18720283974@163.com>
2025-10-31 21:35:51 +0800

    Site updated: 2025-10-31 21:35:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/21/Transfomer/","2025/10/22/changekey/","2025/03/27/decodeapk/","2025/03/01/justrecord/","2024/12/21/someskills/","2025/01/10/nicetools/","2025/01/06/nicearticles/","2025/10/29/【每日早报】/","2024/12/21/张量索引切片操作/"];function toRandomPost(){
+var posts=["2024/12/21/Transfomer/","2025/10/22/changekey/","2025/03/27/decodeapk/","2025/01/06/nicearticles/","2024/12/21/someskills/","2025/01/10/nicetools/","2025/10/31/【每日早报】/","2025/03/01/justrecord/","2024/12/21/张量索引切片操作/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
